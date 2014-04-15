132b25be7856707b19b701a5256e0043ccadbc87
Ian Mejias <ian.mejias@usach.cl>
2014-04-14 23:25:45 -0300

    Modifique el README.md para que mostrara el algoritmo del programa.
    Este "algoritmo" es muy reducido y es solo para resumir de alguna manera
    lo que hablamos en clases.

---
+++ src/xorer_p.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * El orden que se interpretaran los argumentos son los siguientes.
+ * argv[1]: Nombre del archivo que se dividira.
+ * argv[2]: Nro del proceso.
+ * argv[3]: 
+ */
+int main (int argc, char* argv[])
+{
+
+}
