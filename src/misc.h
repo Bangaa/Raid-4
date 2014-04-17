25a1495a3d820e9542f30cbdc9010de39766ebbe
Ian Mejias <ian.mejias@usach.cl>
2014-04-16 23:13:59 -0300

    Se comentan algunas funciones que estaban sin comentario.

--- src/misc.h
+++ src/misc.h
@@ -1,4 +1,8 @@
+#define BASE_ASCII ((int)'0') //<! Valor ASCII del 0 
 
-#define BASE_ASCII ((int)'0')
-
+/**
+ * Funcion que transforma un entero en un string.
+ * @param num Es el entero que se transformara
+ * @return El string terminado en caracter NULL que represente al entero
+ */
 char * inttostring(int num);
--- src/xorerFunct.h
+++ src/xorerFunct.h
@@ -21,6 +21,20 @@ typedef struct
  */
 com_p* new_compipe(int p_in_l, int p_out_l, int p_out_r, int p_in_r);
 
+/**
+ * Comunica los bytes leidos entre procesos hermanos. Hace que el proceso lea
+ * del archivo fname cada cierta tasa de bytes y se los pase al hermano para
+ * que le aplique el XOR. Escribe ademas los bytes leidos en su archivo de 
+ * respaldo, y dependiendo el numero del proceso va a escribir en un archivo de
+ * tipo .part[nro_proceso] o en uno .XOR
+ *
+ * @param fname Es el nombre del archivo del cual va a leer
+ * @param n_proc Es el numero del proceso
+ * @param r_from Indica desde donde leera el proceso
+ * @param nbytes Indica la cantidad de bytes que debe leer
+ * @param pipes Es el puntero a com_p de donde sacara los descriptores de los 
+ * 			pipes que va a utilizar
+ */
 int comunicar(char fname[], int n_proc, long r_from, long nbytes, com_p* pipes );
 
 /**
