5692ad90e45b1ffb659cb30a9e6d95d0301b60af
Ian Mejias <ian.mejias@usach.cl>
2014-04-25 13:40:46 -0300

    Primera version del JOIN
     - Funciona la reconstruccion de archivos, la identificacion de la parte
       que falta y la comunicacion de procesos del JOINer
    
     - Falta agregar la fusion de las partes, para reconstruir el archivo
       original

--- src/joinerFunct.c
+++ src/joinerFunct.c
@@ -2,24 +2,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "joinerFunct.h"
 #include "misc.h"
 
 int missing(char fname[], int total)
 {
 	FILE * sfile;
-	int i_faltante;
+	int i_faltante = 0;
 	int cuantas=0;
-	int fnamelenght = strlen(fname);
-	char sname = (char*) malloc(fnamelenght + 5);
-
-	strcpy(sname, fname);
 
 	for (int i=1; i<= total; i++)
 	{
-		char * num = inttostring(i);
-		strcat(sname, num);
-
-		if ((sfile = fopen(sname, "r")) != NULL)
+		if ((sfile = fopen_part(fname, i, "r")) != NULL)
 			fclose(sfile);
 
 		else
@@ -30,81 +24,40 @@ int missing(char fname[], int total)
 
 		if (cuantas > 1)
 		{
-			free(num);
 			return -1; 
-		}
-
-		sname[fnamelenght] = '\0';
-		free(num);
+		} 
 	}
 
 	return i_faltante; 
 }
 
-int recovery(char fname[], int n_proc, int imissing, int sobra, com_p* pipes )
+int recovery(char fname[], int n_proc, int i_faltante, com_p* pipes )
 {
-	FILE* file_in;	//< Archivo de entrada y salida
+	FILE* file_in;				//< Archivo de entrada y salida
 	void* data_f;				//< Bytes leidos del archivo
 	void* data_p;				//< Bytes leidos del pipe
-	long bytes_left;			//< bytes que me faltan por leer
 	int rdbytes_f, rdbytes_p;	//< Numero de bytes leidos del archivo y pipe
-	char* finname = (char*) malloc(strlen(fname)+10);
 
-	strcpy(finname, fname);
-	strcat(finname, ".XOR");
-
-	if ((file_in = fopen(finname, "r")) != NULL)
+	if (i_faltante != n_proc)
 	{
-		bytes_left = fsof(file_in);
-		fseek(file_in, 8, SEEK_SET);
+		file_in = fopen_part(fname, n_proc, "r");
 	}
 	else
-		return 1;
-
-	if (imissing != n_proc)
 	{
-		fclose(file_in);
-		char * num = inttostring(n_proc);
-		finname[strlen(fname)] = '\0';
-		strcat(finname, num);
-
-
+		file_in = fopen_xor(fname, "r");
+		fseek(file_in, 2*sizeof(int), SEEK_SET);
 	}
-
-
-
-	if (fseek(file_in, r_from, SEEK_SET) != 0)
-		return 1;		// El archivo no es tan largo
 
 	if ((data_f = malloc(TASA)) == NULL)
 		return 1;		// No hay espacio en memoria
 
 	if ((data_p = malloc(TASA)) == NULL)
-		return 1;		// No hay espacio en memoria
+		return 1;		// No hay espacio en memoria 
 
-
-	strcpy(finname, fname);
-	strcat(finname, ".part");
-	strcat(finname, inttostring(n_proc));
-
-	file_out = fopen(finname, "w");
-
-	while (bytes_left > 0)
+	do
 	{
 		rdbytes_f = rdbytes_p = 0;
-		if (!feof(file_in)) // respaldo
-		{ 
-			if (bytes_left >= TASA)
-			{
-				rdbytes_f = fread(data_f, 1, TASA, file_in);
-			}
-			else if (bytes_left < TASA)
-			{
-				rdbytes_f = fread(data_f, 1, bytes_left, file_in);
-			}
-
-			int aux = fwrite(data_f, 1, rdbytes_f, file_out);
-		}
+		rdbytes_f = fread(data_f, 1, TASA, file_in);
 
 		if (pipes->left_in != -1)
 		{
@@ -112,33 +65,67 @@ int recovery(char fname[], int n_proc, int imissing, int sobra, com_p* pipes )
 			write(pipes->left_out, "1", 2);
 		}
 
-		blanquear(data_f, rdbytes_f, TASA);
-		blanquear(data_p, rdbytes_p, TASA);
+		int hndata = (rdbytes_f > rdbytes_p ? rdbytes_f:rdbytes_p);	//numero mas grande de datos
+
+		limpiar(data_f, rdbytes_f, TASA);
+		limpiar(data_p, rdbytes_p, TASA);
 
 		xor_(data_f, data_p, TASA);
 
 		char trash[2];
-		int cuantos = (rdbytes_p >rdbytes_f ? rdbytes_p:rdbytes_f);
 
-		write(pipes->right_out, data_f, cuantos);
+		write(pipes->right_out, data_f, hndata);
 		
-		bytes_left -= cuantos;
-
-		if(bytes_left<=0){//no espera confirmación al enviar el último pack de datos.
+		if(feof(file_in)) 
 			break;
-		}
+
 		else
 		{
 			read(pipes->right_in, trash, 2);
 		}
 
-	} 
+	}
+	while (!feof(file_in));
 
 	free(data_f);
 	free(data_p);
 
 	fclose(file_in);
-	fclose(file_out);
+
+	return 0;
+}
+
+int rebuild(char fname[], int i_faltante, int left_in, int left_out)
+{
+	FILE* file_out, * filexor;
+	long bytes_left;
+	void* data_p;
+	char* foutname = (char*) malloc(strlen(fname)+ 10);
+	char* xorname = (char*) malloc(strlen(fname)+ 5);
+
+	if ((filexor = fopen_xor(fname, "r")) == NULL)
+		return 1;
+
+	if ((data_p = malloc(TASA)) == NULL)
+		return 1;
+
+	bytes_left = fsof(filexor)-2*sizeof(int);
+	fclose(filexor);
+
+	file_out = fopen_part(fname, i_faltante, "w");
+
+	while (bytes_left > 0)
+	{
+		int rdbytes_p;
+
+		rdbytes_p = read(left_in, data_p, TASA);
+		write(left_out, "1", 2);
+		
+		bytes_left-= rdbytes_p;
+		
+		fwrite(data_p, 1, rdbytes_p, file_out); 
+	}
+	fclose(file_out); 
 
 	return 0;
 }
--- src/joinerFunct.h
+++ src/joinerFunct.h
@@ -1,3 +1,4 @@
+#include "xorerFunct.h"
 
 /**
  * Encuantra la parte faltante del archivo fname luego de ser cortado
@@ -5,8 +6,31 @@
  *
  * @param fname Es el nombre del archivo original
  * @param total Es el numero total de partes en que fue cortado fname
- * @return El numero de la parte que falta o -1 si falta mas de una parte
+ * @return El numero de la parte que falta, cero si no falta ninguna o 
+ * -1 si falta mas de una parte.
  *
  *  - Ej. Si falta la parte 3 (<archivo>.part3) devuelve 3
  */
 int missing(char fname[], int total);
+
+/**
+ * Pasa los bytes resultado del XOR entre los archivos existentes para
+ * poder reconstruir el que falta.
+ *
+ * @param fname Es el nombre del fichero original
+ * @param n_proc Es el numero del proceso
+ * @param i_faltante Es el numero de la parte que falta
+ * @param pipes Son los descriptores de los pipes con los cuales se va a
+ * comunicar
+ */
+int recovery(char fname[], int n_proc, int i_faltante, com_p* pipes );
+
+/**
+ * Reconstruye la parte que falta.
+ *
+ * @param fname Es el nombre del archivo original
+ * @param i_faltante Es el numero de la parte que falta
+ * @param left_in Es el descriptor del pipe por donde recibo los bytes
+ * @param left_out Es el descriptor del pipe por donde mando la confirmacion
+ */
+int rebuild(char fname[], int i_faltante, int left_in, int left_out);
