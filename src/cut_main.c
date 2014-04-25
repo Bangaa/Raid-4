9ded14742330d9d990ac4a2f4125399168abab04
Ian Mejias <ian.mejias@usach.cl>
2014-04-25 14:49:06 -0300

    El programa CUT ahora recibe el numero de partes

--- src/cut_main.c
+++ src/cut_main.c
@@ -1,61 +1,68 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "xorerFunct.h"
 #include "misc.h"
-#include <string.h>
-#include <sys/wait.h>
 
 #define _RD 0
 #define _WR 1
 
-#define P 3
 
+/**
+ * argv[1] es el nombre del archivo que se quiere cortar
+ * argv[2] es el numero de partes en que se quiere cortar el archivo
+ */
 int main (int argc, char* argv[])
 {
 
-	if (argc < 2)
+	if (argc < 3)
 	{
 		return 1;
 	}
 
 	FILE* ptrFile ;
-	char fname[strlen(argv[1])+1];  
 	long fsize;
 	long nbytes;
 	int pid;
-	strcpy(fname, argv[1]);
+	int nprocess = atoi(argv[2]);
 
-	ptrFile = fopen(fname, "r");
+	ptrFile = fopen(argv[1], "r");
 
 	if (ptrFile!=NULL)
 	{
 		fsize = fsof(ptrFile);
-		nbytes = fsize/P;
-		if((fsize % P)!=0){// si no es división exacta, le da un byte más.
+		nbytes = fsize/nprocess;
+		if((fsize % nprocess)!=0){// si no es división exacta, le da un byte más.
 			nbytes++;
 		}
 		fclose(ptrFile);
 	}
 	else
 	{
-		printf("Error: No se encontro el archivo <%s>. Operacion abortada\n", fname);
+		printf("Error: No se encontro el archivo <%s>. Operacion abortada\n", argv[1]);
 		return 1;
 	} 
 
-	int** pipes = (int**) malloc(sizeof(int*)*2*P);
+	if ((nprocess==0) || fsize/nprocess == 0)
+	{
+		printf("Numero de partes no permitida \n");
+		return 1;
+	}
 
-	for (int i=0; i< (2*P); i++)
+	int** pipes = (int**) malloc(sizeof(int*)*2*nprocess);
+
+	for (int i=0; i< (2*nprocess); i++)
 	{
 		pipes[i] = (int*) malloc(sizeof(int)*2);
 	}
 
-	for (int i=0; i< (2*P); i++)
+	for (int i=0; i< (2*nprocess); i++)
 	{
 		pipe(pipes[i]);
 	}
 
-	for (int i=0; i< P; i++)
+	for (int i=0; i< nprocess; i++)
 	{
 		pid = fork();
 
@@ -77,7 +84,7 @@ int main (int argc, char* argv[])
 
 			}
 
-			comunicar(fname, i+1, i*nbytes, nbytes, pipesHijo);
+			comunicar(argv[1], i+1, i*nbytes, nbytes, pipesHijo);
 
 			return 0; 
 		}
@@ -85,11 +92,21 @@ int main (int argc, char* argv[])
 
 	// Ejecucion del padre
 	
-	int ok = crear_xor(fname, nbytes, pipes[P*2-2][_RD], pipes[P*2-1][_WR]);
+	int sobra = nbytes*nprocess - fsize ;
+	int ok;
+
+	if ((ptrFile = fopen_xor(argv[1], "w")) == NULL)
+		printf("No se pudo crear el archivo .XOR \n");
+
+	fwrite((void*)&nprocess, sizeof(int), 1, ptrFile);
+	fwrite((void*)&sobra, sizeof(int), 1, ptrFile);
+	fclose(ptrFile);
+	
+	ok = crear_xor(argv[1], nbytes, pipes[nprocess*2-2][_RD], pipes[nprocess*2-1][_WR]);
 
 	if (ok==0)
 	{
-		printf("<%s> fue dividido con exito en %i partes.\n", argv[1], P);
+		printf("<%s> fue dividido con exito en %i partes.\n", argv[1], nprocess);
 	}
 	else
 	{
